61e65dd30bb0c57b85b2cebddd23fbfead12ab4c
flybikeGx <flybike0_0@hotmail.com>
2016-09-11 20:35:30 +0800

    changed to better use, add a template 	new file:   calc_site/calc/templates/home.html 	modified:   calc_site/calc/views.py 	new file:   calc_site/calc2/__init__.py 	new file:   calc_site/calc2/admin.py 	new file:   calc_site/calc2/apps.py 	new file:   calc_site/calc2/migrations/__init__.py 	new file:   calc_site/calc2/models.py 	new file:   calc_site/calc2/tests.py 	new file:   calc_site/calc2/views.py 	modified:   calc_site/calc_site/settings.py 	modified:   calc_site/calc_site/urls.py

--- calc_site/calc/views.py
+++ calc_site/calc/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def add(request,a,b):
-	c = int(a)+int(b)
-	return HttpResponse(str(c))
+def add(request):
+	return render(request, 'home.html')
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
 
 # Create your views here.
--- calc_site/calc_site/urls.py
+++ calc_site/calc_site/urls.py
@@ -19,5 +19,6 @@ from calc import views as calc_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^add/(\d+)\+(\d+)$', calc_views.add, name='add'),
+    url(r'^add/(\d+)\+(\d+)$', calc_views.add2, name='add2'),
+    url(r'^add/$', calc_views.add, name='add')
 ]
